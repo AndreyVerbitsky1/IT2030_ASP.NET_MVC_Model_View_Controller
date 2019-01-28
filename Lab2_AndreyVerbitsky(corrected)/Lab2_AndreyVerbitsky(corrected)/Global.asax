62febfa9079875e19bfaf28c2c5acd93244f28eb
AndreyVerbitsky1 <s01182413@acad.tri-c.edu>
2019-01-27 19:52:02 -0500

    updated

---
+++ Lab2_AndreyVerbitsky(corrected)/Lab2_AndreyVerbitsky(corrected)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab2_AndreyVerbitsky_corrected_.MvcApplication" Language="C#" %>
---
+++ Lab2_AndreyVerbitsky(corrected)/Lab2_AndreyVerbitsky(corrected)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab2_AndreyVerbitsky_corrected_.Startup))]
+namespace Lab2_AndreyVerbitsky_corrected_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
